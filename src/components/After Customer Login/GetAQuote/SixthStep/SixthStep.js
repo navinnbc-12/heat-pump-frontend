6f4c4215e80bc4c6e5417d75c7567e32b18f0cbd
Navin Balachandran <navin@quikieapps.com>
2022-04-21 12:32:49 +0530

    commiting build changes

--- src/components/After Customer Login/GetAQuote/SixthStep/SixthStep.js
+++ src/components/After Customer Login/GetAQuote/SixthStep/SixthStep.js
@@ -5,10 +5,13 @@ import { TailSpin } from "react-loader-spinner";
 import axios from "axios";
 import URL from "../../../../GlobalUrl";
 import globalAPI from "../../../../GlobalApi";
-import { Button, TextField, Typography } from "@mui/material";
+import { Button, TextField, Typography, Box } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import AddIcon from '@mui/icons-material/Add';
-import { fontSize } from "@mui/system";
+import AddIcon from "@mui/icons-material/Add";
+import ChevronRightSharpIcon from "@mui/icons-material/ChevronRightSharp";
+import ChevronLeftSharpIcon from "@mui/icons-material/ChevronLeftSharp";
+import { Card } from "../../../../common";
+
 const useStyles = makeStyles({
   textfield: {
     "& label.Mui-focused": {
@@ -25,23 +28,101 @@ const useStyles = makeStyles({
       },
     },
   },
-  button:{
-    backgroundColor:"black",
-    width:"20%",
-    height:"1.5%",
-    borderRadius:"2.5vw",
-    marginTop:"5%",
-    fontSize:"1vw",
-  }
+  button: {
+    backgroundColor: "black",
+    width: "20%",
+    height: "1.5%",
+    borderRadius: "2.5vw",
+    marginTop: "5%",
+    fontSize: "1vw",
+  },
+  button1: {
+    backgroundColor: "white",
+    width: "20%",
+    height: "1.5%",
+    borderRadius: "2.5vw",
+    marginTop: "5%",
+    fontSize: "1vw",
+    color: "black",
+  },
+  button2: {
+    backgroundColor: "black",
+    width: "20%",
+    height: "1.5%",
+    borderRadius: "2.5vw",
+    marginTop: "5%",
+    fontSize: "1vw",
+    marginLeft: "2%",
+  },
 });
 
 const SixthStep = () => {
   const classes = useStyles();
   const [loader, setLoader] = useState(false);
   const token = JSON.parse(localStorage.getItem("user"));
+  const [inputList, setInputList] = useState([]);
+  const Input = () => {
+    return (
+      <div style={{ marginTop: "2.5%" }}>
+        <TextField
+          label="Room Description"
+          className={classes.textfield}
+          value={""}
+          onChange={""}
+          size="small"
+          InputLabelProps={{
+            style: {
+              fontWeight: "bolder",
+              fontFamily: "outfit",
+              fontSize: "1vw",
+            },
+          }}
+          InputProps={{ style: { fontWeight: "bolder", fontFamily: "outfit" } }}
+        />
+        <TextField
+          label="Radiator(H X W in mm)"
+          className={classes.textfield}
+          value={""}
+          onChange={""}
+          size="small"
+          InputLabelProps={{
+            style: {
+              fontWeight: "bolder",
+              fontFamily: "outfit",
+              fontSize: "1vw",
+            },
+          }}
+          InputProps={{ style: { fontWeight: "bolder", fontFamily: "outfit" } }}
+        />
+        <TextField
+          label="Window(H X W in mm)"
+          className={classes.textfield}
+          value={""}
+          onChange={""}
+          size="small"
+          InputLabelProps={{
+            style: {
+              fontWeight: "bolder",
+              fontFamily: "outfit",
+              fontSize: "1vw",
+            },
+          }}
+          InputProps={{ style: { fontWeight: "bolder", fontFamily: "outfit" } }}
+        />
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    setInputList(inputList.concat(<Input key={inputList.length} />));
+  }, []);
+
+  const onAddBtnClick = (event) => {
+    setInputList(inputList.concat(<Input key={inputList.length} />));
+  };
 
   return (
-    <div className="s6Paper">
+    <Card>
       {loader && (
         <div className="customLoader">
           <TailSpin color="#fa5e00" height="100" width="100" />
@@ -55,64 +136,33 @@ const SixthStep = () => {
       <h4 style={{ fontSize: "1.4vw", marginTop: "6%" }}>
         Radiator and Window Sizes
       </h4>
-      <div style={{marginTop:"2.5%"}}>
-      <TextField
-        label="Room Description"
-        className={classes.textfield}
-        value={""}
-        onChange={""}
-        size="small"
-        InputLabelProps={{
-          style: {
-            fontWeight: "bolder",
-            fontFamily: "outfit",
-            fontSize: "1vw",
-          },
-        }}
-        InputProps={{ style: { fontWeight: "bolder", fontFamily: "outfit" } }}
-      />
-      <TextField
-        label="Radiator(H X W in mm)"
-        className={classes.textfield}
-        value={""}
-        onChange={""}
-        size="small"
-        InputLabelProps={{
-          style: {
-            fontWeight: "bolder",
-            fontFamily: "outfit",
-            fontSize: "1vw",
-          },
-        }}
-        InputProps={{ style: { fontWeight: "bolder", fontFamily: "outfit" } }}
-      />
-      <TextField
-        label="Window(H X W in mm)"
-        className={classes.textfield}
-        value={""}
-        onChange={""}
-        size="small"
-        InputLabelProps={{
-          style: {
-            fontWeight: "bolder",
-            fontFamily: "outfit",
-            fontSize: "1vw",
-          },
-        }}
-        InputProps={{ style: { fontWeight: "bolder", fontFamily: "outfit" } }}
-      />
-      </div>
+      <hr className="s2hr2" />
+      {inputList}
       <Typography>
-      <Button
+        <Button
           className={classes.button}
-          onClick={""}
+          onClick={onAddBtnClick}
           variant="contained"
-          startIcon={<AddIcon style={{height:"5vh",width:"2vw"}}/>}
+          startIcon={<AddIcon style={{ height: "5vh", width: "2vw" }} />}
         >
           Add Room
         </Button>
-        </Typography>
-    </div>
+      </Typography>
+      <Box sx={{ display: "flex" }}>
+        <button variant="contained" className="btn-house btn-icon">
+          <span style={{ height: "27px", width: "27px" }}>
+            <ChevronLeftSharpIcon sx={{ height: "27px", width: "27px" }} />
+          </span>
+          <span style={{ marginLeft: "100px" }}>Previous</span>
+        </button>
+        <button variant="contained" className="btn-house Add btn-icon">
+          <span style={{ marginRight: "100px" }}>Continue</span>
+          <span style={{ height: "27px", width: "27px" }}>
+            <ChevronRightSharpIcon sx={{ height: "27px", width: "27px" }} />
+          </span>
+        </button>
+      </Box>
+    </Card>
   );
 };
 
